55149030df9d700a78b0754a36b602a0b3b05e6b
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-02-22 20:17:29 +0300

    Collaboration packages added

---
+++ Assets/Game Elements/Scripts/SceneNavigation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public void GoToScene(string nameOfScene)
+    {
+        SceneManager.LoadScene(nameOfScene);
+    }
+}
