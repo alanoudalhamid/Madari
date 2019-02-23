8b69b3aa115ffbc1cf88698012ad061ac37ae802
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-02-23 14:41:43 +0300

    FPS Manager added

---
+++ Assets/Game Elements/Scripts/FPS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+    public Text text;
+    private float fps, deltaTime;
+
+    void Start () {
+        deltaTime = 0.0f;
+    }
+	
+
+	void Update () {
+        fps = Mathf.FloorToInt(1.0f / deltaTime);
+        text.text = fps.ToString();
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+}
---
+++ Assets/Game Elements/Scripts/displayFPS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayFPS : MonoBehaviour {
+	public Text text;
+	private float fps, deltaTime;
+
+	// Use this for initialization
+	void Start () {
+		deltaTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fps = Mathf.FloorToInt (1.0f / deltaTime);
+		text.text = fps.ToString ();
+
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		
+	}
+}
