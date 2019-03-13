b0ec022c45b8df73c2b7e613562c05fcecaa8cd9
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-03-13 19:20:55 +0300

    Scripts reimported

---
+++ Assets/Game Elements/Scripts/Question.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public bool asked = false;
+    public string questionText;
+    public Option[] options = new Option[4];
+    Option correctAnswer;
+}
