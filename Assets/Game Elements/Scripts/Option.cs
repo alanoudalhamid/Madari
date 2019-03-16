f9f1209de6ae802a82ee8ccb43613c64b9cd8360
imrehab <rehabq11@gmail.com>
2019-03-17 00:39:38 +0300

    Content UI Jupiter and Earth

---
+++ Assets/Game Elements/Scripts/Option.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour {
+    public string optionText;
+    public bool correct = false;
+
+}
