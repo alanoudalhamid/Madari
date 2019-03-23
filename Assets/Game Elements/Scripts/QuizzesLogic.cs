d2c08c0076a98c85299c35fcb34556b46c6239d0
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-03-23 11:22:05 +0300

    Questions imported

--- Assets/Game Elements/Scripts/QuizzesLogic.cs
+++ Assets/Game Elements/Scripts/QuizzesLogic.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 using ArabicSupport;
 
 public class QuizzesLogic : MonoBehaviour {
-    public int score = 0, askedQuestions = 0;
+    int score = 0, askedQuestions = 0;
     public static int NumOfQuestions;
-    public Text QuestionBox, scoreBox, FinalScoreBox;
-    public Question currentQuestion;
+    public Text QuestionBox, scoreBox;
+    Question currentQuestion;
     public Question[] questions = new Question[NumOfQuestions];
-    public Option[] CurrentOptions = new Option[4];
+    Option[] CurrentOptions = new Option[4];
     public Button[] Buttons = new Button[4];
     public GameObject NextButton;
-    //public GameObject SkipButton;
+    public GameObject SkipButton;
     public Text[] OptionsText = new Text[4];
     public AudioSource correct, wrong;
     public GameObject StartScreen;
@@ -22,67 +22,50 @@ public class QuizzesLogic : MonoBehaviour {
 
      public void StartQuiz()
     {
-        /*StartScreen.SetActive(false);
+        StartScreen.SetActive(false);
         ResultScreen.SetActive(false);
-        QuizScreen.SetActive(true);*/
+        QuizScreen.SetActive(true);
         askedQuestions = 0;
-        for(int i=0; i< questions.Length; i++)
+        for(int i=0; i< NumOfQuestions;i++)
         {
             questions[i].GetComponent<Question>().asked = false;
         }
-        scoreBox.text = score.ToString();
+        scoreBox.text = "0";
         GetQuestion();
     }
 
     public void GetQuestion() {
-        //Debug.Log("inside method");
         int QuizIndex;
         NextButton.SetActive(false);
-        //SkipButton.SetActive(true);
-        if (askedQuestions <= 10 && askedQuestions< questions.Length)
+        SkipButton.SetActive(true);
+        if (askedQuestions != NumOfQuestions)
         {
-            Debug.Log("inside if");
-            
-            //Debug.Log("Quiz index= " + QuizIndex);
-            for (int i = 0; i < 100; i++)
+            QuizIndex = Random.Range(0, NumOfQuestions - 1);
+            if (!questions[QuizIndex].GetComponent<Question>().asked)
             {
-                QuizIndex = Random.Range(0, questions.Length);
-                Debug.Log("generated number= "+ QuizIndex);
-                if (questions[QuizIndex].GetComponent<Question>().asked == false)
+                currentQuestion = questions[QuizIndex];
+                currentQuestion.GetComponent<Question>().asked = true;
+                QuestionBox.text = ArabicFixer.Fix(currentQuestion.GetComponent<Question>().questionText);
+                for (int i = 0; i < 4; i++)
                 {
-                    //Debug.Log("increaing");
-                    askedQuestions++;
-                   // Debug.Log("inside if2");
-                    currentQuestion = questions[QuizIndex];
-                    currentQuestion.GetComponent<Question>().asked = true;
-                    QuestionBox.text = ArabicFixer.Fix(currentQuestion.GetComponent<Question>().questionText);
-                    for (int j = 0; j < 4; j++)
-                    {
-                        //Debug.Log("inside for");
-                        CurrentOptions[j] = currentQuestion.GetComponent<Question>().options[j];
-                        OptionsText[j].text = ArabicFixer.Fix(CurrentOptions[j].GetComponent<Option>().optionText);
-                        Buttons[j].GetComponent<Button>().enabled = true;
-                        Buttons[j].GetComponent<Image>().color = new Color(255, 255, 255, 255);
-                    }
-                    Debug.Log("breaking");
-                    break;
+                    CurrentOptions[i] = currentQuestion.GetComponent<Question>().options[i];
+                    OptionsText[i].text = ArabicFixer.Fix(CurrentOptions[i].GetComponent<Option>().optionText);
                 }
+                askedQuestions++;
             }
         }
         else
         {
             QuizScreen.SetActive(false);
             ResultScreen.SetActive(true);
-            FinalScoreBox.text= score.ToString();
         }
     }
-    public void Answer(Button op)
+    public void Answer(Option op)
     {
         int OptionIndex = 0;
         int CorrectOptionIndex=0;
         for (int i = 0; i < 4; i++) {
-            string OptionText = op.GetComponentInChildren<Text>().text;
-            if (CurrentOptions[i].GetComponent<Option>().optionText.Equals(OptionText))
+            if(CurrentOptions[i] == op)
             {
                 OptionIndex = i;
             }
@@ -104,12 +87,6 @@ public class QuizzesLogic : MonoBehaviour {
             Buttons[CorrectOptionIndex].GetComponent<Image>().color = Color.green;//specify shades
             wrong.Play();
         }
-        for(int i = 0; i < 4; i++)
-        {
-            //Debug.Log("disabling");
-            Buttons[i].GetComponent<Button>().enabled = false;
-        }
-        NextButton.SetActive(true);
     }
     public void ExitQuiz()
     {
