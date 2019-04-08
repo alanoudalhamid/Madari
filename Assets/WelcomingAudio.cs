889ea688f516de37b71c3182fc2e132fc33369b7
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-04-08 14:02:02 +0300

    Welcome Audio Added

---
+++ Assets/WelcomingAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomingAudio : MonoBehaviour {
+    public AudioSource welcome;
+    private static int timesPlayed = 0;
+	// Use this for initialization
+	void Start () {
+        if (timesPlayed == 0)
+        {
+            welcome.Play();
+            timesPlayed++;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
